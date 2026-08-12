9dad93ac83864040d61cea3e2e344a0cb6d79f61
pranav-kulshreshtha <pranavkul4444@gmail.com>
2026-08-12 11:30:20 +0530

    Adding spring redis caching config to location-service

--- services/location-service/src/main/java/com/msp/config/RedisConfig.java
+++ services/location-service/src/main/java/com/msp/config/RedisConfig.java
@@ -1,4 +1,57 @@
 package com.msp.config;
 
-public class RedisConfig {
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.DefaultTyping;
+import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import tools.jackson.databind.jsontype.PolymorphicTypeValidator;
+
+import java.time.Duration;
+import java.util.Map;
+
+@Configuration
+public class RedisConfig implements CachingConfigurer {
+
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        PolymorphicTypeValidator typeValidator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Object.class)
+                .build();
+
+        JsonMapper jsonMapper = JsonMapper.builder()
+                .activateDefaultTypingAsProperty(
+                        typeValidator,
+                        DefaultTyping.NON_FINAL,
+                        "@class")
+                .build();
+
+        GenericJacksonJsonRedisSerializer serializer =
+                new GenericJacksonJsonRedisSerializer(jsonMapper);
+
+        RedisCacheConfiguration defaults = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(serializer))
+                .disableCachingNullValues();
+
+        Map<String, RedisCacheConfiguration> cacheConfigs = Map.of(
+                "airports", defaults.entryTtl(Duration.ofHours(6)),
+                "airportsByCity",defaults.entryTtl(Duration.ofHours(6)),
+                "allAirports", defaults.entryTtl(Duration.ofHours(6)),
+                "cities", defaults.entryTtl(Duration.ofHours(6)),
+                "citiesByCode", defaults.entryTtl(Duration.ofHours(6))
+        );
+
+        return RedisCacheManager.builder()
+                .cacheDefaults(defaults.entryTtl(Duration.ofHours(6)))
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
+    }
 }
